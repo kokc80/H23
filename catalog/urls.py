bca7ca4c3c98a758e6251effc21da211f800f5a6
Kokc80 <kokc@inbox.ru>
2026-03-12 06:15:43 +0900

    исправил ошибки

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("", CatalogHomeView.as_view(), name="base"),
     path("contacts/", CatalogContactsView.as_view(), name="contacts"),
     path("product_list/", CatalogListView.as_view(), name="product_list"),
-    path("product_detail/<int:product_id>/", CatalogDetailView.as_view(), name="product_detail"),
+    path("product_detail/<int:pk>/", CatalogDetailView.as_view(), name="product_detail"),
 ]
