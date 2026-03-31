70096d03a96797c85781a3c1fd7e6fc4d2b4f8f7
Kokc80 <kokc@inbox.ru>
2026-04-01 08:51:04 +0900

    задача 27_1

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,19 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+#EMAIL_HOST_USER = "maks.kuvshinov97@yandex.ru"
+EMAIL_HOST_USER = "npp80@yandex.ru"
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
+LOGIN_REDIRECT_URL = "catalog:home"
+LOGOUT_REDIRECT_URL = "catalog:home"
+LOGIN_URL = "users:login"
