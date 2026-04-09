ce9f3a70ed5cd4a874f910c8ca1e7d854ce9a861
kokc80 <kokc@inbox.ru>
2026-04-09 08:24:23 +0300

    начало проекта 4

--- config/settings.py
+++ config/settings.py
@@ -151,3 +151,11 @@ SERVER_EMAIL = EMAIL_HOST_USER
 LOGIN_REDIRECT_URL = "catalog:home"
 LOGOUT_REDIRECT_URL = "catalog:home"
 LOGIN_URL = "users:login"
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
